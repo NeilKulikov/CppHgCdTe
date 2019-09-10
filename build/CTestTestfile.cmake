9da137937226756d5d4bc91eeb7b9808db438cb7
NeilKulikov <neilkulikov@gmail.com>
2019-09-10 20:28:50 +0300

    Gitignore v2

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/neilkulikov/Documents/SWork/task001b
-# Build directory: /Users/neilkulikov/Documents/SWork/task001b/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("build/test")
